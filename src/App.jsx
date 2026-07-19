6c49e6fae93eabbe2f0c69911741676c4fc35573
Shrikant114 <skntkush114@gmail.com>
2026-07-20 00:06:41 +0530

    Add custom cursor and Animation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Experience from "./components/Experience.jsx";
 import { Contect } from './components/Contect';
 import { Footer } from './components/Footer';
 import { Projects } from './components/Projects';
+import SplashCursor from './components/SplashCursor';
 import './App.css';
 
 function App() {
@@ -24,6 +25,17 @@ function App() {
 
   return (
     <>
+      <SplashCursor
+        DENSITY_DISSIPATION={3.5}
+        VELOCITY_DISSIPATION={2}
+        PRESSURE={0.1}
+        CURL={3}
+        SPLAT_RADIUS={0.2}
+        SPLAT_FORCE={6000}
+        COLOR_UPDATE_SPEED={10}
+        SHADING
+        RAINBOW_MODE={true}
+      />
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <About darkMode={darkMode} />
       <SocialMedia darkMode={darkMode} />
